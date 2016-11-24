4f4a1333544f26bb40812a1429d1d1086b51a172
Anton Koshevoy <anton.koshevoy@gameloft.com>
2016-11-24 16:42:25 +0200

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+    public class PlayerController : NetworkBehaviour
+    {
+    public float speed = 1;
+
+    private Rigidbody rb;
+    private bool startInputOnObject;
+    private Vector2 startPos;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        if (startInputOnObject)
+        {
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                if (Input.touchCount > 0)
+                {
+                    Touch touch = Input.touches[0];
+                    switch (touch.phase)
+                    {
+                        case TouchPhase.Began:
+                            startPos = touch.position;
+                            break;
+                        case TouchPhase.Ended:
+                            rb.AddForce(new Vector3(startPos.x - touch.position.x, 0, startPos.y - touch.position.y) * speed);
+                            startInputOnObject = false;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        startInputOnObject = true;
+    }
+}
