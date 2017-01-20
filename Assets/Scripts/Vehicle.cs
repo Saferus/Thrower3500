b7142f50fc0fbb8f1b7a0afbe9e6a13faa1b026a
yevgeniy.zamkovenko <evgeniyjames@gmail.com>
2017-01-20 12:31:33 +0200

    -make smooth Vehicle turning. Add basic seek behavior and steering

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -4,67 +4,81 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {
+    [SerializeField] private Transform trajectory;
 
-    [SerializeField]
-    private Transform trajectory;
+    [Header("Physics")] [SerializeField] private float acceleration;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float maxForce;
+    [SerializeField] private Vector3 velocity;
+    [SerializeField] private Vector3 steering;
 
-    [SerializeField]
-    private float speed;
+    [Header("Movemental")] [SerializeField] private float changeDistance;
+    [SerializeField] private RoadPoint waypoint;
 
-    [SerializeField]
-    private float changeDistance;
-
-    [SerializeField]
-    private RoadPoint waypoint;
-   
     private List<Transform> waypoints;
 
-	// Use this for initialization
-	void Start ()
-	{
-	    var transformChildCount = trajectory.transform.childCount;
+    private Vector3 directionToPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        var transformChildCount = trajectory.transform.childCount;
 
         waypoints = new List<Transform>(transformChildCount);
 
-	    for (int i = 0; i < transformChildCount; i++)
-	    {
-	        var child = trajectory.GetChild(i);
-	        waypoints.Add(child);
-	    }
+        for (int i = 0; i < transformChildCount; i++)
+        {
+            var child = trajectory.GetChild(i);
+            waypoints.Add(child);
+        }
     }
-	
-	// Update is called once per frame
-	void FixedUpdate ()
-	{
-	    float dt = speed*Time.fixedDeltaTime;
 
-	    float distance = Vector3.Distance(waypoint.transform.position, transform.position);
-
-	    Debug.Log(distance);
-
-	    if (distance < changeDistance)
-	    {
-	        NextWayPoint();
-	    }
-
-	    var directionToPoint = GetDirectionToPoint().normalized;
-
-	    Vector3 movement = new Vector3(directionToPoint.x * dt, 0f, directionToPoint.z * dt);
-	    transform.position += movement;
-	}
-
-    private Vector3 GetDirectionToPoint()
+    // Update is called once per frame
+    void FixedUpdate()
     {
-        return waypoint.transform.position - transform.position;
+//        DebugWaypoint();
+        Accelerate();
+
+        float distance = Vector3.Distance(waypoint.transform.position, transform.position);
+
+        if (distance < changeDistance)
+        {
+            NextWayPoint();
+        }
+
+        transform.position += velocity;
+    }
+
+    private void DebugWaypoint()
+    {
+        Debug.DrawLine(transform.position, waypoint.transform.position, Color.gray);
+    }
+
+    private void Accelerate()
+    {
+        Vector3 diseredVelocity = GetDirectionToPoint(waypoint.transform.position).normalized * maxSpeed;
+//        Debug.DrawLine(transform.position, transform.position + diseredVelocity, Color.blue);
+
+        steering = diseredVelocity - velocity;
+        steering = steering.normalized*maxForce;
+//        Debug.DrawLine(transform.position, transform.position + steering, Color.green);
+
+        velocity += steering;
+//        Debug.DrawLine(transform.position, transform.position + velocity, Color.red);
+
+        velocity.y = 0;
+        transform.position += velocity;
+
+        transform.LookAt(transform.position + velocity);
+    }
+
+    private Vector3 GetDirectionToPoint(Vector3 target)
+    {
+        return target - transform.position;
     }
 
     private void NextWayPoint()
     {
         waypoint = waypoint.nextPoint[0];
-
-        var directionToPoint = GetDirectionToPoint();
-        directionToPoint.x = 0;
-        directionToPoint.z = 0;
-        transform.LookAt(waypoint.transform.position);
     }
 }
